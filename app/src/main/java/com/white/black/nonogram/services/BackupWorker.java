f63e73085038b47648316c00fba90944b5e5faf7
Gal Bachar <gal.bachar.data@gmail.com>
2022-10-31 19:08:44 +0200

    alarms every 6 hours (1.97)

--- app/src/main/java/com/white/black/nonogram/services/BackupWorker.java
+++ app/src/main/java/com/white/black/nonogram/services/BackupWorker.java
@@ -1,15 +1,23 @@
 package com.white.black.nonogram.services;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
-import androidx.work.ListenableWorker;
+import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-public class BackupWorker extends Worker {
+import com.white.black.nonogram.R;
+import com.white.black.nonogram.activities.MenuActivity;
 
-    private static final String TAG = "BackupWorker";
+public class BackupWorker extends Worker {
 
     public BackupWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -18,7 +26,43 @@ public class BackupWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        //call methods to perform background task
-        return ListenableWorker.Result.success();
+        nextAlarm();
+        displayNotification();
+
+        return Result.success();
+    }
+
+    private void displayNotification() {
+        NotificationManager manager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel channel = new NotificationChannel("notif",
+                "notif", NotificationManager.IMPORTANCE_DEFAULT);
+        manager.createNotificationChannel(channel);
+
+        // create and display a notification
+        Intent intent_main = new Intent(getApplicationContext(), MenuActivity.class);
+        PendingIntent pendingIntentMain = PendingIntent.getActivity(getApplicationContext(), 0, intent_main, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "notif")
+                .setContentTitle(getApplicationContext().getString(R.string.new_puzzles))
+                .setContentText(getApplicationContext().getString(R.string.click_to_play))
+                .setContentIntent(pendingIntentMain)
+                .setSmallIcon(R.drawable.icon_white_512)
+                .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.puzzle_green_512))
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setOngoing(false)
+                .setAutoCancel(true);
+
+        manager.notify(1, builder.build());
+    }
+
+    private void nextAlarm() {
+        Intent _intent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, _intent, PendingIntent.FLAG_IMMUTABLE);
+        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        final long TIME_TO_WAIT_MILLISECONDS = 1000 * 3600 * 6; // 6 hours
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TIME_TO_WAIT_MILLISECONDS, pendingIntent);
     }
 }
--- app/src/main/java/com/white/black/nonogram/services/MyService.java
+++ app/src/main/java/com/white/black/nonogram/services/MyService.java
@@ -1,10 +1,12 @@
 package com.white.black.nonogram.services;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.IBinder;
@@ -23,6 +25,8 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        nextAlarm();
+
         String id = "default_channel_id";
         String title = getString(R.string.app_name);
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -55,5 +59,14 @@ public class MyService extends Service {
         stopForeground(false);
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void nextAlarm() {
+        Intent _intent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, _intent, PendingIntent.FLAG_IMMUTABLE);
+        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        final long TIME_TO_WAIT_MILLISECONDS = 1000 * 3600 * 6; // 6 hours
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + TIME_TO_WAIT_MILLISECONDS, pendingIntent);
     }
 }
